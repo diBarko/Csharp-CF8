5feff287fe8dce13ff4604a981536c266042364f
Denis Barko <denisbarko@hotmail.com>
2025-10-01 20:27:56 +0300

    feat: add expressions app

--- ExpressionsApp/Program.cs
+++ ExpressionsApp/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num1 = 1;
+            int num2 = 2;
+            int sum = 0;
+            int sub = 0;
+            int mul = 0;
+            int div = 0;
+            int mod = 0;
+            int result = 0;
+            int finalResult = 0;
+
+            sum = num1 + num2;
+            sub = num1 - num2;
+            mul = num1 * num2;
+            div = num1 / num2;
+            mod = num1 % num2;
+
+            result = sum++;
+            result = ++sub;
+            result += sum;
+            result -= 5;
+            result *= 2;
+            result /= 2;
+
+            finalResult = (num1 + num2) * (num2 / num1++) % num2;
+
+            Console.WriteLine($"{finalResult}");
+
+
         }
     }
 }
