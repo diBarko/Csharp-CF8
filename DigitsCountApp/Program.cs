1986d3a43c4277acfbd1e8909c4bb126e81766e1
Denis Barko <denisbarko@hotmail.com>
2025-10-05 23:35:37 +0300

    feat: add digits count app

---
+++ DigitsCountApp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DigitsCountApp
+{
+    /// <summary>
+    /// Μετράει τον αριθμό των ψηφίων ενός ακεραίου που δίνει ο χρήστης.
+    /// π.χ. το 135 -> 3
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempNum = 0;
+            int digits = 0;
+
+            Console.WriteLine("Παρακαλώ εισάγετε έναν ακέραιο αριθμό:");
+            if (!int.TryParse(Console.ReadLine(), out int num)) {
+                Console.WriteLine("Σφάλμα κατά την εισαγωγή.");
+                return;
+            }
+
+            tempNum = num;
+
+            do
+            {
+                tempNum /= 10;
+                digits++;
+            } while (tempNum > 0);
+
+            Console.WriteLine($"Digits count of {num} : {digits}.");
+        }
+    }
+}
