1b68361a9ddb6e1fbc6ea97ed2dcb6f166f52667
Denis Barko <denisbarko@hotmail.com>
2025-10-05 23:13:57 +0300

    feat: add while  app

---
+++ CharReadDoWhileApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CharReadDoWhileApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int charCode;
+            char inputChar;
+
+            do
+            {
+                charCode = Console.Read();
+                inputChar = (char)charCode;
+                Console.WriteLine($"char: {inputChar} - charCode: {charCode}");
+            } while (charCode != 10);// || charCode != 13); // Enter key (CR or LF)
+        }
+    }
+}
