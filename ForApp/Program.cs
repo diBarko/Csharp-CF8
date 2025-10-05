24011d96b3576765f72316512c6380f136417c92
Denis Barko <denisbarko@hotmail.com>
2025-10-05 23:15:41 +0300

    feat: add for app

---
+++ ForApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ForApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum  = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"Άθροισμα μονών: {sum}");
+
+            sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0) continue;
+                sum += i;
+            }
+
+            Console.WriteLine($"Άθροισμα μονών: {sum}");
+        }
+    }
+}
