0f0a65ab996f2054e3a7089bc96a5707ec0a352a
Denis Barko <denisbarko@hotmail.com>
2025-10-05 22:52:18 +0300

    feat add big int and operator overloading demo

---
+++ BigIntegerApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace BigIntegerApp
+{
+    /// <summary>
+    /// Υπολογίζει το γινόμενο 1*2*3*...*n
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            BigInteger result = 1;  // auto-boxing και υπερφόρτωση του = για BigInteger
+            int i = 1;
+            while (i <= n)
+            {
+                result *= i;
+                i++;
+            }
+
+            Console.WriteLine($"Το 1*2*3*...*{n} ({n}!) = {result:n0}");
+        }
+    }
+}
