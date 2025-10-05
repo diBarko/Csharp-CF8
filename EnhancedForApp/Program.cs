c79b8ed3167b2e980f507607520052f7e67670db
Denis Barko <denisbarko@hotmail.com>
2025-10-05 23:15:59 +0300

    feat: add enhanced for app

---
+++ EnhancedForApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EnhancedForApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "Athens", "London", "Paris" };
+            foreach (string city in cities)
+            {
+                Console.Write(city + " ");
+            }
+        }
+    }
+}
