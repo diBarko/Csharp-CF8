10bb2806e1e4d8398bbc759d978c109e2b8ec219
Denis Barko <denisbarko@hotmail.com>
2025-10-05 23:23:31 +0300

    feat: add exception demo app

---
+++ FormatExceptionApp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FormatExceptionApp
+{
+    /// <summary>
+    /// Διαβάζει ένα string εισόδου και προσπαθεί
+    /// να το μετατρέψει σε int κι ελέγχει με try/catch
+    /// για FormatException.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            while (true)
+            {
+             
+                try
+                {
+                    Console.Write("Δώσε έναν ακέραιο αριθμό (0 για έξοδο) : ");
+                    num = int.Parse(Console.ReadLine()!);
+                    if (num == 0) break;
+                    Console.WriteLine($"Εισάγατε τον ακέραιο: {num}");
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"Σφάλμα: {e.Message}\nΜη έγκυρη μορφή αριθμού. Προσπάθησε ξανά.");
+                }
+            }
+        }
+    }
+}
