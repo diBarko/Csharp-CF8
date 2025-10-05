9e8841ff6e6d2d7520d769f0038d3d5fca123038
Denis Barko <denisbarko@hotmail.com>
2025-10-05 23:46:12 +0300

    feat: add swap demo

---
+++ SwapApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SwapApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 2;
+            Console.WriteLine($"Initial: (a[{a}], b[{b}])");
+            Swap(a, b);
+            Console.WriteLine($"Swap: (a[{a}], b[{b}])");
+            SwapWithTuple(a, b);
+            Console.WriteLine($"Swap with tuple: (a[{a}], b[{b}])");
+        }
+
+        public static void SwapWithTuple(int a, int b)
+        {
+            (a, b) = (b, a);
+        }
+
+        public static void Swap(int a, int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+}
