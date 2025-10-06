2657ed53f73002a4c6a150e3febf58f057e4d0f6
Denis Barko <denisbarko@hotmail.com>
2025-10-06 20:30:51 +0300

    feat: add palindrome demo

---
+++ PalindromeApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PalindromeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3, 2, 1 };
+            bool isPalindrome = true;
+
+            for (int i = 0, j = ints.Length - 1; i < j; i++, j--)
+            {
+                if (ints[i] != ints[j])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            Console.WriteLine($"Is palindrome: {isPalindrome}");
+
+        }
+    }
+}
