b4ec1204e0d045737790911ba1f804e151c5c62f
Denis Barko <denisbarko@hotmail.com>
2025-10-06 22:02:12 +0300

    feat: add string (non-)nullable | interning demo

---
+++ StringApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StringApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = null;              // non-nullable
+            string? s2 = null;             // nullable     implies to static analyzer that this could be null
+            string s3 = "AUEB";     // immutable
+            string s4 = "AUEB";     // interning, more like scp in java
+            string s5 = new string("AUEB");
+        }
+    }
+}
