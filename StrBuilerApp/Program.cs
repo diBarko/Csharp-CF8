aa1836904c93ab52c0cb7eecbdfdca05e5a8584e
Denis Barko <denisbarko@hotmail.com>
2025-10-07 00:01:04 +0300

    feat: add String Builder demo

---
+++ StrBuilerApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace StrBuilerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            Console.WriteLine(DateTime.Now);
+            sb.Append("Number: ");
+
+            for (int i = 0; i < 100_000_000; i++)
+            {
+                sb.Append(i);
+            }
+
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(sb.ToString().Substring(0, 128));
+        }
+    }
+}
