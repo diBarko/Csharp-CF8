a1bcf39a189a641b359773a2d0fc06d28ce9cbed
Denis Barko <denisbarko@hotmail.com>
2025-10-06 18:52:42 +0300

    feat: add array demo app

---
+++ ArraysApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ArraysApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+            int[] arr2 = {1, 2, 3};
+
+            arr[0] = 0;
+            arr2[1] = 5;
+
+            Console.WriteLine($"Length of arr: {arr.Length}");
+            Console.WriteLine($"Arr[2] = {arr[2]}");
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.WriteLine( arr2[i] );
+            }
+
+            foreach (int el in arr2)
+            {
+                Console.WriteLine( el );
+            }
+        }
+    }
+}
