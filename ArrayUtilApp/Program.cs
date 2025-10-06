59ed39fbff7190a04faf048d107674e74d09de29
Denis Barko <denisbarko@hotmail.com>
2025-10-06 21:07:05 +0300

    feat: add Array utility class - shallow copy demo

---
+++ ArrayUtilApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ArrayUtilApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4 };
+            int[] dest = new int[arr.Length];
+
+            Array.Copy(arr, dest, arr.Length);
+
+            foreach (int el in dest)
+            {
+                Console.Write(el + " ");
+            }
+        }
+    }
+}
