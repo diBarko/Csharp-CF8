0ca817eac1a5d6cbd8cfa3d5d4ba46a341e3306f
Denis Barko <denisbarko@hotmail.com>
2025-10-06 20:13:02 +0300

    feat: add array min demo

---
+++ ArrayMin/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ArrayMin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ages = { 15, 25, 12, 55 };
+            int minPosition = 0;
+
+            for (int i = 0; i < ages.Length; i++)
+            {
+                if (ages[i] < ages[minPosition])
+                {
+                    minPosition = i;
+                }
+            }
+            Console.WriteLine($"Min Position: {minPosition + 1}, value: {ages[minPosition]}");
+        }
+    }
+}
