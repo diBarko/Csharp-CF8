a1ea4529ccdd7102ee0bc91a02ae1839b1a2235c
Denis Barko <denisbarko@hotmail.com>
2025-10-19 00:05:58 +0300

    feat: add abstract classes

---
+++ AbstractClassesApp/AbstractAnimal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesApp
+{
+    internal abstract class AbstractAnimal
+    {
+        public int id { get; set; }
+        public string? Name { get; set; }
+        public double Age{ get; set; }
+
+        // using abstract keyword, we force derived classes to implement these methods.
+        public abstract override string ToString();
+
+        public abstract void Speak();
+
+        public virtual void Eat()
+        {
+            Console.WriteLine("Animal is eating.");
+        }
+    }
+}
---
+++ AbstractClassesApp/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesApp
+{
+    internal class Cat : AbstractAnimal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Cat says meow!");
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {id}, Name: {Name}, Age: {Age}";
+        }
+    }
+}
