6f8611441215893fdbf66ba4662cc23a7d4798ff
Denis Barko <denisbarko@hotmail.com>
2025-10-18 23:53:19 +0300

    feat: add shallow deep copies demo app

---
+++ ShallowDeepCopyApp/Model/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShallowDeepCopyApp.Model
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        private string? LastName { get; set; }
+        public Address? StudAddress { get; set; }
+
+        public Student GetReference() => this;
+
+        public Student GetShallowCopy() => (Student)MemberwiseClone();  // field-by-field copy
+
+        public Student GetDeepCopy()
+        {
+            Student student = (Student)MemberwiseClone();
+            student.StudAddress = new Address()
+            {
+                Street = StudAddress?.Street,
+                Number = StudAddress?.Number,
+                ZipCode = StudAddress?.ZipCode
+            };
+
+            return student;
+        }
+
+        public Student(Student student)
+        {
+            Id = student.Id;
+            FirstName = student.FirstName;
+            LastName = student.LastName;
+            StudAddress = new Address()
+            {
+                Street = student.StudAddress?.Street,
+                Number = student.StudAddress?.Number,
+                ZipCode = student.StudAddress?.ZipCode
+            };
+        }
+    }
+}
---
+++ ShallowDeepCopyApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ShallowDeepCopyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
