e11beccd51666817d34821bef66ab4eea0aaec18
Denis Barko <denisbarko@hotmail.com>
2025-10-19 15:02:02 +0300

    feat: Dependency Injection

---
+++ DependencyInjectionApp/MovingSpace.cs
@@ -0,0 +1,21 @@
+﻿using InterfacesApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionApp
+{
+    internal class MovingSpace
+    {
+        public IMovable? Movable { get; set; }
+
+        public MovingSpace(IMovable? movable)
+        {
+            Movable = movable;
+        }
+
+        public void MakeMove5() => Movable!.Move5();
+    }
+}
